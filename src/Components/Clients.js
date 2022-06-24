b6cf3896b7692ee86c69505bcdd931f7fe3cab8d
mu-gamal <mu.gamal.elkhaiat@gmail.com>
2022-06-25 01:53:41 +0200

    Handle clients swiper

--- src/Components/Clients.js
+++ src/Components/Clients.js
@@ -1,5 +1,5 @@
 import { useTranslation } from "react-i18next";
-import { Carousel } from "react-bootstrap";
+import { Swiper, SwiperSlide } from "swiper/react";
 import Header from "./Header";
 import Template from "./Template";
 import Photo from "./Photo";
@@ -7,51 +7,46 @@ import homeTown from "../Images/homeTown.png";
 import geekCampus from "../Images/geekCampus.png";
 import nfr from "../Images/nfr.png";
 import homy from "../Images/homy.png";
-import summit1 from "../Images/summit1.png";
-import summit2 from "../Images/summit2.png";
+import summit from "../Images/summit.png";
 import about_background from "../Images/about_background.svg";
 import styles from "../Main.module.scss";
+import "swiper/css";
 
 function Clients() {
   const { t } = useTranslation();
   return (
-    <Template
-      background={about_background}
-      itemsContainerStyle={{ paddingBottom: 70 }}
-      containerStyle={styles.background_cover_with_hover}
-      itemStyle={{
-        width: `${100 / (window.innerWidth > 992 ? 6 : window.innerWidth > 768 ? 3 : 2)
-          }%`,
-        marginTop: 70,
-      }}
-      header={<Header color="#C65FAA" bgColor="#C65FAA" title={t("clients")} />}
-      items={[
-        // <div className={styles.items}>
-        <Carousel >
-          <Carousel.Item >
-            <Photo src={homeTown} />,
-          </Carousel.Item>
-          <Carousel.Item >
-            <Photo src={geekCampus} />,
-          </Carousel.Item>
-          <Carousel.Item >
-            <Photo src={nfr} />,
-          </Carousel.Item>
+    <>
+      <Template
+        items={[]}
+        background={about_background}
+        itemsContainerStyle={{ paddingBottom: 40 }}
+        containerStyle={styles.background_cover_with_hover}
+        header={<Header color="#C65FAA" bgColor="#C65FAA" title={t("clients")} />}
+      />
 
-          <Carousel.Item >
-            <Photo src={homy} />,
-          </Carousel.Item>
-          <Carousel.Item >
-            <Photo src={summit1} />,
-          </Carousel.Item>
-          <Carousel.Item >
-            <Photo src={summit2} />,
-          </Carousel.Item>
-        </Carousel>
-        // </div >
-
-      ]}
-    />
+      <Swiper
+        spaceBetween={50}
+        className={styles.wrapper}
+        style={{ width: "90%", paddingBottom: 70 }}
+        slidesPerView={window.innerWidth > 992 ? 5 : window.innerWidth > 768 ? 3 : 2}
+      >
+        <SwiperSlide>
+          <Photo src={homeTown} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Photo src={geekCampus} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Photo src={nfr} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Photo src={homy} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Photo src={summit} />
+        </SwiperSlide>
+      </Swiper>
+    </>
   );
 }
 
